ddc64cdf57241daf274ad4b5f280ce1703493cad
Puneet Chaturvedi <puneet.chaturvedi1990@gmail.com>
2018-05-22 22:34:25 -0400

    test files
    test files

---
+++ HelloWorld.rb
@@ -0,0 +1,34 @@
+class HelloWorld
+
+    def firstMethod
+        puts "hello"
+        puts "World"
+        your_name = "Puneet"
+        puts "Hello #{your_name}"
+        puts 'ruby test'
+        
+    end
+
+   def secondMethod
+        ary = [  "fred", 10, 3.14, "This is a string", "last element", ]
+        ary.each do |i|
+        puts i
+        end
+        
+        colors = { "red" => 0xf00, "green" => 0x0f0, "blue" => 0x00f }
+        colors.each do |key, value|
+        print key, " is ", value, "\n"
+        end
+
+        (10..15).each do |n| 
+            print n, ' ' 
+         end
+   end
+
+   
+end
+
+obj= HelloWorld.new
+
+obj.firstMethod
+obj.secondMethod
---
+++ Person.rb
@@ -0,0 +1,17 @@
+class Person
+    def initialize(id, name)
+        @name=name
+        @id=id
+        puts 
+    end
+    
+    def display
+        puts "my id is #@id and name is #{@name}"
+       
+    end
+end
+
+obj= Person.new(1, "puneet")
+
+obj.display()
+obj.display
